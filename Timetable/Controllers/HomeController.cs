af324641ad21e114605385ccbee003706fa15a69
Митин Алексей <drakon9854@gmail.com>
2022-02-20 13:00:46 +0300

    Ver 0.1 Upd: добавил удаление, изменение и доп.инфу

--- Timetable/Controllers/HomeController.cs
+++ Timetable/Controllers/HomeController.cs
@@ -26,18 +26,76 @@ namespace Timetable.Controllers
         {
             return View(await db.Teachers.ToListAsync());
         }
+
         public IActionResult Create()
         {
             return View();
         }
 
-        [HttpPost][Authorize]
+        [HttpPost]
         public async Task<IActionResult> Create(Teacher teacher)
         {
             db.Teachers.Add(teacher);
             await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            return RedirectToAction("Teacher");
         }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Teacher teacher = await db.Teachers.FirstOrDefaultAsync(p => p.Id_Teacher == id);
+                if (teacher != null)
+                    return View(teacher);
+            }
+            return NotFound();
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Teacher teacher = await db.Teachers.FirstOrDefaultAsync(p => p.Id_Teacher == id);
+                if (teacher != null)
+                    return View(teacher);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Teacher teacher)
+        {
+            db.Teachers.Update(teacher);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Teacher");
+        }
+
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Teacher teacher = await db.Teachers.FirstOrDefaultAsync(p => p.Id_Teacher == id);
+                if (teacher != null)
+                    return View(teacher);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id != null)
+            {
+                Teacher teacher = new Teacher { Id_Teacher = id.Value };
+                db.Entry(teacher).State = EntityState.Deleted;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Teacher");
+            }
+            return NotFound();
+        }
+
         public IActionResult index()
         {
             return View();
